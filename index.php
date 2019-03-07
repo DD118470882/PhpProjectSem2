e3984f74877b2a5317abbd8032dfbb0dd8e16c51
Dilla <Dilla@DESKTOP-BGG0JB6>
2019-03-07 15:44:42 +0000

    Project Commit

--- index.php
+++ index.php
@@ -53,7 +53,7 @@ body {
 						<!-- Collect the nav links, forms, and other content for toggling -->
 						<div class="collapse navbar-collapse offset" id="navbarSupportedContent">
 							<ul class="nav navbar-nav menu_nav ml-auto">
-								<li class="nav-item active"><a class="nav-link" href="home.html">Home</a></li>  
+								<li class="nav-item active"><a class="nav-link" href="index.php">Home</a></li>  
 								<li class="nav-item"><a class="nav-link" href="Interests.html">Interests</a></li>
                                                                 <li class="nav-item"><a class="nav-link" href="ConsultingScroller.html">Shop</a></li>
 							</ul>
